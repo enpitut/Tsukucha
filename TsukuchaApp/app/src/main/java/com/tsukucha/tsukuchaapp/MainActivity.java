d1294e594a978be56757bd59b46e6f784e2dcf41
Daiki-Nakamura <nakamuradaiki@My-MacBook-Pro.local>
2015-08-20 15:06:57 +0900

    8.20 change by N

--- TsukuchaApp/app/src/main/java/com/tsukucha/tsukuchaapp/MainActivity.java
+++ TsukuchaApp/app/src/main/java/com/tsukucha/tsukuchaapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView)findViewById(R.id.webView);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://cookpad.com/");
     }
 
     @Override
