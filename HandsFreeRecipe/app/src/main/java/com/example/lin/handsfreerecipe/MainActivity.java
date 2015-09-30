a58d17b288ccbda0f17b8331e708fa4baea86eab
LIN WEI <rinnuxei@gmail.com>
2015-09-30 17:05:51 +0900

    Add scrolling on text

--- HandsFreeRecipe/app/src/main/java/com/example/lin/handsfreerecipe/MainActivity.java
+++ HandsFreeRecipe/app/src/main/java/com/example/lin/handsfreerecipe/MainActivity.java
@@ -12,10 +12,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
@@ -28,56 +30,71 @@ import java.util.Timer;
 import java.util.concurrent.CountDownLatch;
 
 public class MainActivity extends AppCompatActivity implements RecognitionListener {
-    ToggleButton btnStart; //press to start cooking
-    TextView recognizedWord; //recognized results
-    ProgressBar voiceRMS; //voice rms bar
-    ListView suggestedList; //suggested results list
-    SpeechRecognizer lisener = null;
-    Intent recognizer;
-    private String LOG_TAG = "HandsFreeRecipe"; //for debug info
-    CountDownTimer mTimer = null; //timer
+    private Button btnStart;
+    private TextView recognizedWord;
+    private ProgressBar voiceRMS;
+    private TextView simpleList;
+    private SpeechRecognizer speechRecognizer = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "HandsFreeRecipe";
+    private CountDownTimer mTimer = null;
     private AudioManager mAudioManager;
     private int mStreamVolume = 0;
-
+    private boolean started = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //disable sleep
         mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        btnStart = (ToggleButton) findViewById(R.id.start_cooking);
+        //The Button to switch start/end speech recognition
+        btnStart = (Button) findViewById(R.id.start_cooking);
+        btnStart.setText("Start cooking!");
+        //The ProgressBar to visualize the voice
         voiceRMS = (ProgressBar) findViewById(R.id.voice_rms);
         voiceRMS.setVisibility(View.INVISIBLE);
+        //The TextView to hold the recognized words
         recognizedWord = (TextView) findViewById(R.id.recognition_result);
-        suggestedList = (ListView) findViewById(R.id.suggested_words);
-        lisener = SpeechRecognizer.createSpeechRecognizer(this);
-        lisener.setRecognitionListener(this);
-        recognizer = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recognizer.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "jp");
-        recognizer.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
-        recognizer.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                             RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        recognizer.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
-        btnStart.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-
+        //The TextView to be tested scrolling on
+        simpleList = (TextView) findViewById(R.id.suggested_words);
+        simpleList.setMovementMethod(new ScrollingMovementMethod());
+        // Initialize the SpeechRecognizer object
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(this);
+        //Create recognizerIntent and set some extra info
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "ja");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
+        //Start speech recognition when
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView,
-                                         boolean isChecked) {
-                if (isChecked) {
+            public void onClick(View arg0) {
+                if(started == false){
+                    btnStart.setText("Cooking...");
+                    //Show the voice visualization
                     voiceRMS.setVisibility(View.VISIBLE);
                     voiceRMS.setIndeterminate(true);
-                    lisener.startListening(recognizer);
+                    speechRecognizer.startListening(recognizerIntent);
+                    //Mute the sound effect while start listening
                     mStreamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
                     mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
-                } else {
+                    started = true;
+                }
+                else {
+                    started = false;
+                    recognizedWord.setText("You just said...");
+                    btnStart.setText("Start cooking!");
                     Log.d(LOG_TAG, "Stop Listening");
                     voiceRMS.setIndeterminate(false);
                     voiceRMS.setVisibility(View.INVISIBLE);
-                    lisener.stopListening();
+                    speechRecognizer.stopListening();
+
                 }
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -108,8 +125,8 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
     protected void onPause() {
         super.onPause();
-        if (lisener != null) {
-            lisener.destroy();
+        if (speechRecognizer != null) {
+            speechRecognizer.destroy();
             Log.i(LOG_TAG, "destroy");
         }
 
@@ -117,6 +134,7 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
     @Override
     public void onReadyForSpeech(Bundle params) {
+        //Restore the sound when speech has begun
         mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, mStreamVolume, 0);
         Log.i(LOG_TAG, "onReadyForSpeech:Cancel Timer");
         if(mTimer != null)
@@ -133,7 +151,7 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
     @Override
     public void onRmsChanged(float rmsdB) {
-        //Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
+        Log.i(LOG_TAG, "onRmsChanged: " + rmsdB);
         voiceRMS.setProgress((int) rmsdB);
     }
 
@@ -146,47 +164,57 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
     public void onEndOfSpeech() {
             Log.i(LOG_TAG, "onEndOfSpeech");
             voiceRMS.setIndeterminate(true);
-            btnStart.setChecked(false);
+            speechRecognizer.stopListening();
     }
 
     @Override
     public void onError(int errorCode) {
-        String errorMessage = getErrorText(errorCode);
-        Log.d(LOG_TAG, "FAILED " + errorMessage);
-        recognizedWord.setText(errorMessage);
-        lisener.cancel();
-        lisener.startListening(recognizer);
+        if(started == true){
+            String errorMessage = getErrorText(errorCode);
+            Log.d(LOG_TAG, "FAILED " + errorMessage);
+            recognizedWord.setText(errorMessage);
+            if(mTimer != null)
+                mTimer.cancel();
+            speechRecognizer.cancel();
+            restartListen();
+        }
     }
 
     @Override
     public void onResults(Bundle results) {
-        if(mTimer != null)
-            mTimer.cancel();
         Log.i(LOG_TAG, "onResults");
         ArrayList<String> matches = results
                 .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
         String text = "";
-        for (String result : matches)
-            text += result + "\n";
-
+        for (String result : matches){
+            if(result.equals("上") || result.equals("うえ") || result.equals("up")){
+                text = "上にスクロール";
+                simpleList.scrollBy(0,-48);
+                break;
+            }
+            if(result.equals("下") || result.equals("した") || result.equals("down")){
+                text = "下にスクロール";
+                simpleList.scrollBy(0,48);
+                break;
+            }
+            if(result.equals("左") || result.equals("ひだり")|| result.equals("left")){
+                text = "左にスクロール";
+                simpleList.scrollBy(-48, 0);
+                break;
+            }
+            if(result.equals("右" )|| result.equals("みぎ") || result.equals("right")){
+                text = "右にスクロール";
+                simpleList.scrollBy(+48, 0);
+                break;
+            }
+        }
         recognizedWord.setText(text);
-
+        btnStart.setText("Recognition finished");
         //Start listening again
         Log.i(LOG_TAG, "OnResults:Start listening again");
-        if(mTimer == null){
-            mTimer = new CountDownTimer(2000,2000){
-                @Override
-                public void onTick(long millisUntilFinished) {
-
-                }
-                @Override
-                public void onFinish() {
-                    Log.d("Restart","Timer.onFinish: Time's up,restart listening");
-                    btnStart.setChecked(true);
-                }
-            };
-        }
-        mTimer.start();
+        if(mTimer != null)
+            mTimer.cancel();
+        restartListen();
     }
 
     @Override
@@ -239,8 +267,31 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
         super.onDestroy();
         if(mTimer != null)
            mTimer.cancel();
-        if(lisener !=null)
-            lisener.destroy();
+        if(speechRecognizer !=null)
+            speechRecognizer.destroy();
+    }
+
+    //Restart listening while one recognition finishes or error occurs
+    public void restartListen (){
+        if(mTimer == null){
+            mTimer = new CountDownTimer(1000,1000){
+                @Override
+                public void onTick(long millisUntilFinished) {}
+                @Override
+                public void onFinish() {
+                    Log.d("Restart","Timer.onFinish: Time's up,restart listening");
+                    btnStart.setText("Cooking...");
+                    //Show the voice visualization
+                    voiceRMS.setVisibility(View.VISIBLE);
+                    voiceRMS.setIndeterminate(true);
+                    speechRecognizer.startListening(recognizerIntent);
+                    //Mute the sound effect while start listening
+                    mStreamVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+                }
+            };
+        }
+        mTimer.start();
     }
 
 
